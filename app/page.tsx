8058733968e53c011e538abb23a021f43f944e15
Tanmay <tankver11@gmail.com>
2026-01-20 19:44:50 +0530

    fix base url error

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,15 @@ import ExploreBtn from "@/components/ExploreBtn";
 import EventCard from "@/components/EventCard";
 import {IEvent} from "@/database";
 import {cacheLife} from "next/cache";
+import events from "@/lib/constants";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const Page = async () => {
     'use cache';
     cacheLife('hours')
-    const response = await fetch(`${BASE_URL}/api/events`);
-    const {events} = await response.json();
+    // const response = await fetch(`${BASE_URL}/api/events`);
+    // const {events} = await response.json();
 
     return (
         <section>
@@ -22,7 +23,7 @@ const Page = async () => {
                 <h3>Featured Events</h3>
 
                 <ul className="events">
-                    {events && events.length > 0 && events.map((event: IEvent) => (
+                    {events && events.length > 0 && events.map((event) => (
                         <li key={event.title} className="list-none">
                             <EventCard {...event} />
                         </li>
